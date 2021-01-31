7f96127a57b44cf5d4ab8ae263be517ab2ad7990
Monirul-Navy <76878536+Monirul-Navy@users.noreply.github.com>
2021-02-01 02:19:13 +0600

    final upload

---
+++ script.js
@@ -0,0 +1,78 @@
+function priceCalculation(){
+    const firstInput = document.getElementById('first-Input').value;
+    const firstNumber = parseInt(firstInput);
+    const economyInput = document.getElementById('economy-Input').value;
+    const economyNumber = parseInt(economyInput);
+    
+    let subTotal = (firstNumber * 150) + (economyNumber * 100)
+    document.getElementById('subtotal').innerText = subTotal;
+
+    let withVat = subTotal * .1;
+    document.getElementById('vat').innerText = withVat;
+
+    let total = subTotal + withVat;
+    document.getElementById('total').innerText = total;
+}
+
+function HandleTicketCounting(ticketClass,isIncrease){
+    const input = document.getElementById(ticketClass + '-Input').value;
+    const inputNumber = parseInt(input);
+    let ticketCount = inputNumber;
+    let subTotal = 0;
+    if(isIncrease === true){
+        ticketCount =  ticketCount + 1;
+    }if(isIncrease === false &&  ticketCount > 0){
+        ticketCount =  ticketCount - 1;
+    }
+    document.getElementById(ticketClass + '-Input').value =  ticketCount;
+    priceCalculation();
+}
+
+document.getElementById('printDetails').style.display = 'none'
+document.getElementById('full-body').style.display = 'block';
+document.getElementById('body').style.background = 'block';
+
+document.getElementById('bookingBtn').addEventListener("click", function(){
+    // confirm("Are You Confirm This Purchase")
+    document.getElementById('full-body').style.display = 'none';
+    document.getElementById('body').style.background = 'none';
+    document.getElementById('printDetails').style.display = 'block'
+
+    // firstClassPassenger 
+    const firstClassPassenger = document.getElementById('first-Input').value;
+    document.getElementById('firstClassPassenger').innerText = firstClassPassenger
+
+    // economyClassPassenger
+    const economyClassPassenger = document.getElementById('economy-Input').value;
+    document.getElementById('economyClassPassenger').innerText = economyClassPassenger
+
+    //subTotal Value Set
+    const subtotal = document.getElementById('subtotal').innerText
+    document.getElementById('tableSubTotal').innerText = subtotal;
+
+    //table Vat set
+    const vat = document.getElementById('vat').innerText;
+    document.getElementById('tableVat').innerText = vat;
+
+    // table Total set
+    const total = document.getElementById('total').innerText;
+    document.getElementById('tableTotal').innerText = total;
+
+    //starting Place Set
+    const start_input = document.getElementById('start-Input').value;
+    document.getElementById('start').innerText = start_input;
+
+    // Return Place Set 
+    const end_input = document.getElementById('end-Input').value;
+    document.getElementById('end').innerText = end_input;
+
+    // journey Date 
+    const start_Date_Input = document.getElementById('start-Date-Input').value;
+    document.getElementById('startingDate').innerText = start_Date_Input;
+
+    // return Date 
+    const return_Date_Input = document.getElementById('return-Date-Input').value;
+    document.getElementById('returningDate').innerText = return_Date_Input;
+})
+
+
